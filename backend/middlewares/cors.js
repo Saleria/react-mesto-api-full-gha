a4fecb4d6429f0f68e395e2411e450d3caf047f5
Valeriya K <saleria-v@mail.ru>
2024-02-01 23:25:43 +0300

    fix cors

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -11,12 +11,10 @@ const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 module.exports = (req, res, next) => {
   const { origin } = req.headers;
   const { method } = req;
-  if (allowedCors.includes(origin)) {
-    if (allowedCors.includes(origin)) {
-      res.header('Access-Control-Allow-Origin', origin);
-    }
-  }
   const requestHeaders = req.headers['access-control-request-headers'];
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
