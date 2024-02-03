4d657984c2e447686145edee032284680ec0a79f
Valeriya K <saleria-v@mail.ru>
2024-02-03 19:02:28 +0300

    fix cors

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,15 +1,15 @@
-const allowedCors = [
-  'https://mesto.saleria.nomoredomainsmonster.ru',
-  'https://api.mesto.saleria.nomoredomainsmonster.ru',
-  'http://mesto.saleria.nomoredomainsmonster.ru',
-  'http://localhost:3000',
-  'https://localhost:3000',
-];
+// const allowedCors = [
+// 'https://mesto.saleria.nomoredomainsmonster.ru',
+// 'https://api.mesto.saleria.nomoredomainsmonster.ru',
+// 'http://mesto.saleria.nomoredomainsmonster.ru',
+// 'http://localhost:3000',
+// 'https://localhost:3000',
+// ];
 
 const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
 module.exports = (req, res, next) => {
-  const { origin } = req.headers;
+// const { origin } = req.headers;
   const { method } = req;
   res.set('Access-Control-Allow-Origin', '*');
   const requestHeaders = req.headers['access-control-request-headers'];
