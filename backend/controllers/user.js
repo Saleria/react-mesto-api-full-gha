ad72a38a1834936f6ebfda028df3650cf60512be
Valeriya K <saleria-v@mail.ru>
2024-02-03 07:56:37 +0300

    change ENV

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,7 +1,7 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
-const { NODE_ENV, JWT_SECRET } = process.env;
+// const { NODE_ENV, JWT_SECRET } = process.env;
 const User = require('../models/user');
 const { OK, CREATED_OK } = require('../responses/responses');
 const NotFoundError = require('../errors/not-found-err');
@@ -105,7 +105,7 @@ module.exports.login = (req, res, next) => {
   return User.findUserByCredentials(email, password)
     .then((user) => {
       // создадим токен
-      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
+      const token = jwt.sign({ _id: user._id }, 'some-secret-key', { expiresIn: '7d' });
       // вернём токен
       res.send({ token });
     })
