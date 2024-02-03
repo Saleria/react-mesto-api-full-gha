5c116d598ad716de988cc3365a490df734d87af3
Valeriya K <saleria-v@mail.ru>
2024-02-03 05:56:02 +0300

    del cors

--- backend/app.js
+++ backend/app.js
@@ -9,36 +9,14 @@ const { createUser } = require('./controllers/user');
 const { loginValidatoin, createUserValidation } = require('./middlewares/validator');
 const errorHandler = require('./middlewares/error-handler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-// const cors = require('./middlewares/cors');
+const cors = require('./middlewares/cors');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
-const allowedCors = [
-  'https://mesto.saleria.nomoredomainsmonster.ru',
-  'https://api.mesto.saleria.nomoredomainsmonster.ru',
-  'http://mesto.saleria.nomoredomainsmonster.ru',
-  'http://localhost:3000',
-  'https://localhost:3000',
-];
-
-const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-
-app.use((req, res, next) => {
-  const { origin } = req.headers;
-  const { method } = req;
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
-  const requestHeaders = req.headers['access-control-request-headers'];
-  if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    return res.end();
-  } return next();
-});
+// app.use(cors);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
