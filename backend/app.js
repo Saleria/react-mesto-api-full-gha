5d277b096b17e5e226fcd30e38182901447096ae
Valeriya K <saleria-v@mail.ru>
2024-02-03 04:31:15 +0300

    Revert "fix env"
    This reverts commit 291e0b04a7e4228e27aa8af25c0ec7d66d2e3fc1.

--- backend/app.js
+++ backend/app.js
@@ -16,13 +16,12 @@ const app = express();
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
-app.use(cors);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //l
 app.use(requestLogger);
-
+app.use(cors);
 app.get('/crash-test', () => {
   setTimeout(() => {
     throw new Error('Сервер сейчас упадёт');
