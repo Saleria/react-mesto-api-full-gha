cad87372b7ab7f34ceed85d9add543c2ba25f1ae
Valeriya K <saleria-v@mail.ru>
2024-02-03 04:20:06 +0300

    fix app

--- backend/app.js
+++ backend/app.js
@@ -19,6 +19,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// logger
 app.use(requestLogger);
 app.use(cors);
 app.get('/crash-test', () => {
